da262db61f361829a279f594bf28bb863cad7d76
Buyaki <rittahbuyaki@gmail.com>
2023-02-13 12:12:04 +0300

    Hello {handleNameChange()} only works upon page reload

--- src/Content.js
+++ src/Content.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const Content = () => {
+  const [name, setName] = useState('Dave');
   const handleNameChange = () => {
     const names = ['Bob', 'Kevin', 'Dave'];
     const int = Math.floor(Math.random() * 3);
@@ -9,10 +12,29 @@ const Content = () => {
     console.log('You clicked it')
   }
 
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`)
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
+
   return(
     <main className="mt-8 ml-5 text-center">
-      <p>Hello {handleNameChange()}!</p>
-      <button onClick={handleClick}>Click it</button>
+      <p onDoubleClick={handleClick}> Hello {handleNameChange()}!</p>
+      
+      <div className="mt-3">
+        <button onClick={handleClick}>Click it</button>
+      </div>
+
+      <div className="mt-3">
+        <button onClick={() => handleClick2('Dave')}>Click it</button>
+      </div>
+
+      <div className="mt-3">
+        <button onClick={(e) => handleClick3(e)}>Click it</button>
+      </div>
     </main>
   )
 }

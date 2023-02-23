c50c8a5086bd510e9e0cf55197e61e44d50ea5eb
Buyaki <rittahbuyaki@gmail.com>
2023-02-23 11:51:30 +0300

    Add try, catch and finally in useEffect

--- src/App.js
+++ src/App.js
@@ -6,15 +6,38 @@ import Content from "./Content";
 import Footer from "./Footer";
 
 function App() {
+  const API_URL = 'http://localhost:3500/items'
   const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || [])
 
   const [newItem, setNewItem] = useState('')
 
   const [search, setSearch] = useState('')
 
+  const [fetchError, SetFetchError] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    localStorage.setItem('shoppinglist', JSON.stringify(items));
-  }, [items])
+    
+    const fetchItems = async () => {
+      try{
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive expected data')
+        const listItems = response.json();
+        setItems(listItems);
+        SetFetchError(null)
+      }catch (err) {
+        SetFetchError(err.message)
+      }finally{
+        setIsLoading(false);
+      }
+    }
+    
+    setTimeout(() => {
+      (async () => await fetchItems())();
+    }, 2000)
+    
+  }, [])
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -55,11 +78,15 @@ function App() {
         setSearch={setSearch}
       />
 
-      <Content
-        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck}
-        handleDelete={handleDelete}
-      />
+      <main>
+        {isLoading && <p> Loading Items... </p>}
+        {fetchError && <p className="ml-3 mt-3" style={{color: "red"}} >{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
 
       <Footer length={items.length}/>
     </div>
--- src/Content.js
+++ src/Content.js
@@ -2,17 +2,19 @@ import ItemList from "./ItemList";
 
 const Content = ({items, handleCheck, handleDelete}) => {
   return(
-    <main className="mt-8 ml-5 text-center">
-      {items.length ? (
-        <ItemList
-          items={items}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-        />
-      ) : (
-        <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
-      )}
-    </main>
+    <>
+      <div  className="mt-8 ml-5 text-center">
+        {items.length ? (
+          <ItemList
+            items={items}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />
+        ) : (
+          <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+        )}
+      </div>
+    </>
   )
 }
 
